b4090fd666265ad024bfe8b927f14728a4884bbd
Pedro Amaral <pedro.amaral@loft.com.br>
2018-12-18 16:07:33 -0200

    Zoeira

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log("Hello World");
 
-console.log("Fix");
+console.log("aaa");
+
+for (var i=0;i<=100;i++){
+    console.log("aaa");
+    while(true){
+        console.log("uhu");
+    }
+}
